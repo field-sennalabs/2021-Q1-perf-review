95969b37473c4c1d71a7e462e68488c969c2b347
pot_sennalabs <pot@sennalabs.com>
2021-03-24 20:22:28 +0700

    feat: create header

--- clound-budget/src/App.js
+++ clound-budget/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import Logo from "./assets/Rectangle 3.jpg";
+import Cloud from "./assets/Vector.svg";
+import Language from "./assets/Language Selection.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="logo-wrapper">
+        <div className="overlay" />
+        <img className="logo" src={Logo} />
+        <header>
+          <span>
+            <img src={Cloud} />
+            CloudBudget
+          </span>
+          <ul>
+            <li>
+              <b>OVERVIEW</b>
+            </li>
+            <li>FEATURES</li>
+            <li>TECHNOLOGY</li>
+            <li>CONTACT</li>
+            <li>Sign up</li>
+          </ul>
+          <div className="menu">
+            <img src={Language} />
+            <button className="login-btn">LOGIN</button>
+          </div>
+        </header>
+
+        <div className="content">
+          <div>CloudBudget</div>
+          <p>
+            Cloud budget management for everyone. Only €69.95 a Month After a 7
+            Day Trial of Up to €4.99
+          </p>
+
+          <button className="signup-btn">SIGN UP</button>
+        </div>
+      </div>
     </div>
   );
 }
